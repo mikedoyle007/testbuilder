8cdb9e625f4bc79bb985b61eef446b7ab2ed1a85
Mike Doyle <mikedoyle007@gmail.com>
2017-06-03 22:29:34 -0700

    Detect Visa, MasterCard, Amex, and Diner's Club cards
    also includes a test suite, to use run the function: 'runTestSuite()'

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,6 +13,70 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+  var prefix = cardNumber.slice(0, 2);
+  var network = '';
+  var cardLength = cardNumber.length;
+
+  var dinersClubLength = 14;  
+  var amexLength = 15;
+  var masterCardLength = 16;
+  var visaLength = [13, 16, 19];
+
+  var dinersClubPrefix = ['38', '39'];
+  var amexPrefix = ['34', '37'];
+  var masterCardPrefix = ['51', '52', '53', '54', '55'];
+  var visaPrefix = '4';
+  
+  if (cardLength === dinersClubLength) {
+  	if (prefix === dinersClubPrefix[0] || prefix === dinersClubPrefix[1]) {
+  		network = "Diner's Club";
+  	}
+  } else if (cardLength === amexLength) {
+  	if (prefix === amexPrefix[0] || prefix === amexPrefix[1]) {
+  		network = "American Express";
+  	}
+  } else if (cardLength === masterCardLength) {
+  	for (var i = 0; i < masterCardPrefix.length; i++) {
+  	  if (prefix === masterCardPrefix[i]) {
+  		network = "MasterCard";
+  	  }	
+  	}
+  	if (prefix[0] === visaPrefix) {
+  		network = "Visa";
+  	}  	
+  } else if (isVisaCardLength(cardLength)) {
+  	if (prefix[0] === visaPrefix) {
+  		network = "Visa";
+  	}
+  } 
+  return network;
 };
 
+function isVisaCardLength(length) {
+  return (length === 13 || length === 16 || length === 19);
+}
+
+// tests
+function assertIsEqual(actual, expected, testName) {
+  if (actual === expected) {
+  	console.log('passed');
+  } else {
+  	console.log('FAILED [' + testName + '] expected ' + expected + ' but got ' + actual);
+  }
+}
+
+function runTestSuite() {
+	assertIsEqual(detectNetwork('38345678901234'),      "Diner's Club",     'detected network as expected');
+	assertIsEqual(detectNetwork('39345678901234'),      "Diner's Club",     'detected network as expected');
+	assertIsEqual(detectNetwork('343456789012345'),     "American Express", 'detected network as expected');
+	assertIsEqual(detectNetwork('373456789012345'),     "American Express", 'detected network as expected');
+	assertIsEqual(detectNetwork('4123456789012'),       "Visa",             'detected network as expected');
+	assertIsEqual(detectNetwork('4123456789012345'),    "Visa",             'detected network as expected');
+	assertIsEqual(detectNetwork('4123456789012345678'), "Visa",             'detected network as expected');
+	assertIsEqual(detectNetwork('5112345678901234'),    "MasterCard",       'detected network as expected');
+	assertIsEqual(detectNetwork('5212345678901234'),    "MasterCard",       'detected network as expected');
+	assertIsEqual(detectNetwork('5312345678901234'),    "MasterCard",       'detected network as expected');
+	assertIsEqual(detectNetwork('5412345678901234'),    "MasterCard",       'detected network as expected');
+	assertIsEqual(detectNetwork('5512345678901234'),    "MasterCard",       'detected network as expected');
+}
 
